fa6b3d12063017d9fa1c110c827d37ad63d50d4e
Adrian <adriandrabik@gmail.com>
2016-02-19 15:59:00 +0100

    First end to end test

--- Tests/OX_OOP_EndToEnd.java
+++ Tests/OX_OOP_EndToEnd.java
@@ -5,6 +5,7 @@ import OX.Table;
 import static org.assertj.core.api.Assertions.*;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 /**
  * Created by adrabik on 18.02.16.
@@ -29,5 +30,26 @@ public class OX_OOP_EndToEnd {
 
         assertThat(table.isWon()).isTrue();
         assertThat(winner).isEqualTo(player1);
+    }
+    @Test
+    public static void endsWithDraw(){
+        Player player1 = Player.playerSelection(Player.X_SIGN, table);
+        Player player2 = Player.playerSelection(Player.O_SIGN, table);
+
+        player1.move("1");
+        player2.move("2");
+        player1.move("3");
+        player2.move("7");
+        player1.move("8");
+        player2.move("9");
+        player1.move("4");
+        player2.move("5");
+        player1.move("6");
+        Player winner = table.winner();
+
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(table.isWon(), false);
+        softAssert.assertNull(winner);
+        softAssert.assertAll();
     }
 }
